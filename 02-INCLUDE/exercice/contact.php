6c4f4c05d363fe7972f5cacc78939b1b527cd24e
Astrid JONATHAN <astrid.jonathan@gmail.com>
2019-10-18 15:43:46 -0400

    W du 18/10
    notions à Header

---
+++ 02-INCLUDE/exercice/contact.php
@@ -0,0 +1,13 @@
+<?php
+     include_once './inc/header.php';
+?>  
+    <main>
+        <h1> Contact ! </h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est eum suscipit deleniti,
+         autem fuga ad consequatur sequi temporibus harum placeat magnam rem corrupti 
+         impedit veniam fugit modi voluptates distinctio at!</p>
+    </main>
+                     
+    <?php
+     include_once './inc/footer.php';
+?>
---
+++ 02-INCLUDE/exercice/index.php
@@ -0,0 +1,18 @@
+
+    <!-- Inclusion du Header -->
+    <?php
+     include_once './inc/header.php';
+     ?>
+
+     <!-- Mon contenu -->
+    <main>
+    <h1> Bienvenue dans cette formation PHP ! </h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est eum suscipit deleniti,
+         autem fuga ad consequatur sequi temporibus harum placeat magnam rem corrupti 
+         impedit veniam fugit modi voluptates distinctio at!</p>
+    </main>
+     
+    <!-- Inclusion du footer -->
+    <?php
+     include_once './inc/footer.php';
+     ?>
