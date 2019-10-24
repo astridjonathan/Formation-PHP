af625b97a5ac3c55bffde110b293ae326fc317b1
Astrid JONATHAN <astrid.jonathan@gmail.com>
2019-10-24 11:59:45 -0400

    Create db_contact.sql

---
+++ 06-PDO/db_contact.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `AddContact` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+    `prenom` varchar(80)  NULL,
+    `nom` varchar(80)  NULL,
+    `email` varchar(80) NOT NULL,
+    `password` varchar(256) NOT NULL,
+     `sujet` varchar(256) NOT NULL,
+    `message` text NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
